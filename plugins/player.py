90a0776a3c3f3c9c43cf7abd777e63676e679775
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-05 15:42:02 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -696,11 +696,11 @@ allcmd = ["play", "player", f"play@{U}", f"player@{U}"] + admincmds
 async def not_chat(_, m: Message):
     buttons = [
         [
-            InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/subinps/MusicPlayer'),
-            InlineKeyboardButton('🧩 Source Code', url='https://github.com/subinps/MusicPlayer'),
+            InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/roymusicplay/nonstop-music'),
+            InlineKeyboardButton('🧩 Source Code', url='https://github.com/roymusicplay/nonstop-music'),
         ],
         [
-            InlineKeyboardButton('How to Make', url='https://youtu.be/iBK-5pP2eHM'),
+            InlineKeyboardButton('father', url='https://t.me/abhinasroy'),
             InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help')       
         ]
         ]
